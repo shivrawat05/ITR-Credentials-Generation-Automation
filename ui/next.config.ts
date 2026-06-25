ac3b4c61133e99225792bcf290bc1cdfb7a05a3e
Mani Kant Sharma <manikant.sharma@ampleserv.com>
2026-06-25 16:18:54 +0530

    Use localhost in rewrites to prevent IPv6 binding issues

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -10,10 +10,10 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:4000/:path*"
-      }
+        destination: "http://localhost:4000/:path*",
+      },
     ];
-  }
+  },
 };
 
 export default nextConfig;
